a056e21f81f4086ce209f04edac77a8d8e75eaf4
bencejdanko <bencejdanko@gmail.com>
2025-02-24 13:13:19 -0800

    add sequelize template

---
+++ README.md
@@ -0,0 +1,8 @@
+# Server Setup
+
+## Create an .env file
+
+MYSQL_USER=
+MYSQL_PASSWORD=
+MYSQL_HOST=
+DATABASE=
---
+++ server/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+// const router = require('./books.routes.js');
+
+const app = express();
+
+// // Middleware to parse JSON bodies
+// app.use(express.json());
+
+// // Use the router
+// app.use('/api', router);
+
+// app.listen(5000, function () {
+//     console.log("Server listening on port 5000");
+// });
